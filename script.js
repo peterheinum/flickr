5c82262b910a79b5a99428da248527e7a9cab3c0
peter heinum <peter.heinum@gmail.com>
2018-11-10 19:54:01 +0100

    added some design things and changed suggestions from p to buttons, started doing function for the buttons

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
 const key = "c6172fde54b6b96d7afbd08ad5118a7a";
 let photoArray = [];
 
-const promise = fetch(`https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${key}&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1`)
-    .then((res) => {
-        return res.json();
-    })
-    .then(JsonData => {
-        let tempPhotoArray = JsonData.photos.photo;
-        //generatePhotoArray(tempPhotoArray);
-        //displayImagesFromPhotoArray();
-    })
-    .catch(err => {
-        console.log("Error:", err);
-    })
-
-
-
-
-
 class Photo {
     constructor(id, farm, server, secret) {
         this.id = id;
@@ -26,6 +9,7 @@ class Photo {
         this.secret = secret;
     }
 }
+
 function generatePhotoArray(tempPhotoArray) {
     if (photoArray) {
         photoArray = [];
@@ -43,10 +27,8 @@ function generatePhotoArray(tempPhotoArray) {
 
 function displayImagesFromPhotoArray() {
     var body = document.querySelector('.gallerySpace');
-
-    while(body.firstChild){
-        body.removeChild(body.firstChild);
-    }   
+    cleanElement(body);
+    
     
 
     for (let i = 0; i < 50; i++) {
@@ -59,10 +41,40 @@ function displayImagesFromPhotoArray() {
     }
 }
 
+function generateSuggestions(TagArray){
+    var navbar = document.querySelector('#miniNavbar');
+    //cleanElement(navbar);
+
+    if(TagArray.length != 0){
+        for (let i = 0; i < 10; i++) {                    
+            let tag = TagArray[i]._content;
+            let button = document.createElement("button");
+            button.className = "relatedLink";
+            button.addEventListener("click", followTagLink);
+            button.textContent = tag;
+            navbar.appendChild(button);
+        }
+    }   
+}
+
+function cleanElement(element){
+    while(element.firstChild){ //TODO but not h2 elements!
+        element.removeChild(element.firstChild);
+    }
+}
+
+function followTagLink(event){
+    console.log(event.target.textContent);
+
+}
 
 
 const searchBar = document.querySelector("#searchBar");
 searchBar.addEventListener("keydown", handleSearch);
+
+function fetchPhotos(tag){
+    
+}
 
 function handleSearch(event) {
     if (event.key === 'Enter') {
@@ -70,22 +82,15 @@ function handleSearch(event) {
         const promise = fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${key}&text=${searchBar.value}&tags=${searchBar.value}&format=json&nojsoncallback=1`)
             .then((res) => res.json())
             .then((JsonData) => {
-                generatePhotoArray(JsonData.photos.photo);
+                generatePhotoArray(JsonData.photos.photo);                
                 displayImagesFromPhotoArray();
             }).catch(error => console.log(error));
         const promise2 = fetch(`https://api.flickr.com/services/rest/?method=flickr.tags.getRelated&api_key=${key}&tag=${searchBar.value}&format=json&nojsoncallback=1`)
             .then((res) => {
                 return res.json();
             }).then((JsonData) => {
-                var body = document.querySelector('.navbar-right');
+                generateSuggestions(JsonData.tags.tag)
                 
-                for (let i = 0; i < 10; i++) {                    
-                    let tag = JsonData.tags.tag[i]._content;
-                    let temp = document.createElement("p");
-                    temp.className = "relatedLink";
-                    temp.textContent = tag;
-                    body.appendChild(temp);
-                }
             })
     }
 }
